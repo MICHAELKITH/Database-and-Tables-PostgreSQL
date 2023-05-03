418a1c595683507cd4bec8d49333d493b2a28961
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-05-03 10:36:18 +0300

    Transaction SQL

--- data.sql
+++ data.sql
@@ -1,12 +1,20 @@
-INSERT INTO animals VALUES (1, 'Agumon', '02/03/2020', 0, 'T', 10.23);
-INSERT INTO animals VALUES (2, 'Gabumon', '09/15/2018', 2, 'T', 8);
-INSERT INTO animals VALUES (3, 'Pikachu', '01/07/2021', 1, 'F', 15.04);
-INSERT INTO animals VALUES (4, 'Devimon', '05/12/2017', 5, 'T', 11);
-ALTER TABLE animals add column species char(30);
-INSERT INTO animals VALUES (5, 'Charmandar', '02/08/2020', 0, 'F', -11);
-INSERT INTO animals VALUES (6, 'Plantom', '11/15/2021', 2, 'T', -5.7);
-INSERT INTO animals VALUES (7, 'Squirtle', '04/02/1993', 3, 'F', -12.13);
-INSERT INTO animals VALUES (8, 'Angemon', '06/12/2005', 1, 'T', -45);
-INSERT INTO animals VALUES (9, 'Boarmon', '06/7/2020', 7, 'T', 20.4);
-INSERT INTO animals VALUES (10, 'Blossom', '10/13/1998', 3, 'T', 17);
-INSERT INTO animals VALUES (11, 'Ditto', '05/14/2022', 4, 'F', 22);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (5, 'Charmander', '2022-02-08', 0, 'false', -11);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (6, 'Plantmon', '2022-11-15', 2, 'false', -5.7);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (7, 'Squirtle', '1993-04-02', 3, 'false', -12.13);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (8, 'Angemon', '2005-06-12', 1, 'true', -45);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (9, 'Boarmon', '2005-06-07', 7, 'true', 20.4);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (10, 'Blossom', '1998-10-13', 3, 'true', 17);
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (11, 'Ditto', '2022-05-14', 4, 'true', 22);
--- queries.sql
+++ queries.sql
@@ -1,82 +1,49 @@
-/*Queries that provide answers to the questions
-FROM all projects.*/
-SELECT  *
-FROM animals
-WHERE trim(name) like '%mon';
+/*Queries that provide answers to the questions from all projects.*/
 
-SELECT  *
-FROM animals
-WHERE extract('year'
-FROM date_of_birth) BETWEEN 2016 AND 2019;
+SELECT * FROM animals WHERE name like '%mon'
+SELECT name FROM animals WHERE date_part('year', date_of_birth) >= 2016 and date_part('year', date_of_birth) <= 2019
+SELECT name FROM animals WHERE neutered = true AND escaped_attempts < 3
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu'
+SELECT name, escaped_attempts FROM animals WHERE weight_kg > 10.5
+SELECT * FROM animals WHERE neutered = true
+SELECT * FROM animals WHERE name != 'Gabumon' OR SELECT * FROM animals WHERE name <> 'Gabumon'
+SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3
 
-SELECT  *
-FROM animals
-WHERE neutered = true
-AND escape_attempts < 3;
+/* DAY TWO*/
 
-SELECT  date_of_birth
-FROM animals
-WHERE name IN ('Agumon', 'Pikachu');
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals
 
-SELECT  name,escape_attempts
-FROM animals
-WHERE weight_kg > 10.5;
+ROLLBACK;
+SELECT * FROM animals
 
-SELECT  *
-FROM animals
-WHERE neutered = true;
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals
 
-SELECT  *
-FROM animals
-WHERE name != 'Gabumon';
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals
 
-SELECT  *
-FROM animals
-WHERE weight_kg BETWEEN 10.4 AND 17.3; begin; update animals
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * (-1);
+ROLLBACK TO SP1;
+SELECT * FROM animals
 
-SET species = 'nonspecified';
+UPDATE animals SET weight_kg = weight_kg * (-1) WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals
 
-SELECT  *
-FROM animals; rollback; begin; update animals
-
-SET species = 'digimon'
-WHERE trim(name) like '%mon'; update animals
-
-SET species = 'pokemon'
-WHERE species is NULL; commit; begin; delete
-FROM animals;
-
-SELECT  *
-FROM animals; rollback;
-
-SELECT  *
-FROM animals; begin; delete
-FROM animals
-WHERE date_of_birth > '01/01/2022'; savepoint neg; update animals
-
-SET weight_kg = (weight_kg * -1); rollback to neg; update animals
-SET weight_kg = (weight_kg * -1)
-WHERE weight_kg < 0; release savepoint neg; commit;
-
-SELECT  COUNT(*)
-FROM animals;
-
-SELECT  COUNT(*)
-FROM animals
-WHERE escape_attempts = 0;
-
-SELECT  AVG(weight_kg)
-FROM animals
-
-SELECT  neutered,SUM(escape_attempts) AS total_escape_attempts
-FROM animals
-GROUP BY  neutered
-
-SELECT  species, MAX(weight_kg) AS max_weight, MIN(weight_kg) AS min_weight
-FROM animals
-GROUP BY  species
-SELECT  species,AVG(escape_attempts) AS avg_escapes
-FROM animals
-WHERE extract('year'
-FROM date_of_birth) BETWEEN 1990 AND 2000
-GROUP BY  species
+SELECT count(*) FROM animals
+SELECT count(*) FROM animals where escaped_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escaped_attempts) as excaped_attempts FROM animals GROUP BY neutered
+SELECT species, MIN(weight_kg) as minWeight, MAX(weight_kg) as maxWeight FROM animals GROUP BY species
+SELECT species, AVG(escaped_attempts) as avgEscapes FROM animals WHERE date_part('year', date_of_birth) 
+BETWEEN 1990 AND 2000 GROUP BY species

65f2140e09a03df98f2fc6d99329cde6f21877aa
Dev Mike <97337681+MICHAELKITH@users.noreply.github.com>
2023-05-09 17:47:08 +0300

    join tables index and foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,55 +1,56 @@
-CREATE DATABASE clinic;
-USE clinic;
-CREATE TABLE medical_histories(
-    id INTEGER NOT NULL,
-    admitted_at TIMESTAMP(0) NOT NULL,
-    patient_id INTEGER NOT NULL,
-    status VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    medical_histories ADD PRIMARY KEY(id);
-    
-    SELECT * FROM medical_histories;
-CREATE TABLE invoice_items(
-    id INTEGER NOT NULL,
-    unit_price DECIMAL(8, 2) NOT NULL,
-    quantity INTEGER NOT NULL,
-    total_price DECIMAL(8, 2) NOT NULL,
-    invoice_id INTEGER NOT NULL,
-    treatment_id INTEGER NOT NULL
-);
-ALTER TABLE
-    invoice_items ADD PRIMARY KEY(id);
-CREATE TABLE treatments(
-    id INTEGER NOT NULL,
-    type VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    treatments ADD PRIMARY KEY(id);
-CREATE TABLE patients(
-    id INTEGER NOT NULL,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
     date_of_birth DATE NOT NULL
 );
-ALTER TABLE
-    patients ADD PRIMARY KEY(id);
-CREATE TABLE invoices(
-    id INTEGER NOT NULL,
-    total_amount DECIMAL(8, 2) NOT NULL,
-    generated_at TIMESTAMP(0) NOT NULL,
-    payed_at TIMESTAMP(0) NOT NULL,
-    medical_history_id INTEGER NOT NULL
+
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admitted_at timestamp NOT NULL,
+    patient_id int NOT NULL,
+    status varchar(50) NOT NULL
 );
-ALTER TABLE
-    invoices ADD PRIMARY KEY(id);
-ALTER TABLE
-    invoices ADD CONSTRAINT invoices_medical_history_id_foreign FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id);
-ALTER TABLE
-    invoice_items ADD CONSTRAINT invoice_items_treatment_id_foreign FOREIGN KEY(treatment_id) REFERENCES treatments(id);
-ALTER TABLE
-    invoice_items ADD CONSTRAINT invoice_items_invoice_id_foreign FOREIGN KEY(invoice_id) REFERENCES invoices(id);
-ALTER TABLE
-    treatments ADD CONSTRAINT treatments_id_foreign FOREIGN KEY(id) REFERENCES medical_histories(id);
-ALTER TABLE
-    medical_histories ADD CONSTRAINT medical_histories_patient_id_foreign FOREIGN KEY(patient_id) REFERENCES patients(id);
+
+CREATE TABLE medical_histories_has_treatments (
+    medical_history_id int references medical_histories(id),
+    treatment_id int references treatments(id)
+);
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL NOT NULL,
+    generated_at timestamp NOT NULL,
+    payed_at timestamp NOT NULL,
+    medical_history_id int NOT NULL
+);
+
+CREATE TABLE treatments (
+    id SERIAL PRIMARY KEY,
+    type varchar(50) NOT NULL,
+    name varchar(50) NOT NULL
+);
+
+CREATE TABLE invoice_items (
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL NOT NULL,
+    quantity int NOT NULL,
+    total_price DECIMAL NOT NULL,
+    invoice_id int NOT NULL,
+    treatment_id int NOT NULL
+);
+
+ALTER TABLE invoice_items 
+ADD FOREIGN KEY (invoice_id) REFERENCES invoices (id);
+CREATE INDEX ON invoice_items (invoice_id);
+
+ALTER TABLE invoice_items 
+ADD FOREIGN KEY (treatment_id) REFERENCES treatments (id);
+CREATE INDEX ON invoice_items (treatment_id);
+
+ALTER TABLE invoices 
+ADD FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id);
+CREATE INDEX ON invoices (medical_history_id);
+
+ALTER TABLE medical_histories 
+ADD FOREIGN KEY (patient_id) REFERENCES patients (id);
+CREATE INDEX ON medical_histories (patient_id);
